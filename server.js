69a7c6a143e6f3d9ca0be57e253635651fe94aa3
wardo923 <72359682+wardo923@users.noreply.github.com>
2026-02-28 22:32:42 -0400

    fix routes + health

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const server = http.createServer((req, res) => {
       res.writeHead(200, { "Content-Type": "application/json" });
       return res.end(JSON.stringify({ ok: true, received: true }));
     });
-    return;
+    return; 
   }
 
   // Anything else
